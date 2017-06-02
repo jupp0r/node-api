da008c93f34f83014d13052a409d4650e6111ec6
Hendrik Sollich <hendrik@hoodie.de>
2017-06-02 16:50:47 +0200

    workspacified
    use `cargo build --all` to build:
      * *node-api*
      * *tests*
      * *node-api-sys*
      * *examples/hello-world*

--- examples/hello-world/src/lib.rs
+++ examples/hello-world/src/lib.rs
@@ -4,15 +4,13 @@ extern crate node_api;
 
 napi_module!("testmod", register);
 
-use node_api::{NapiEnv, NapiValue, FromNapiValues, ToNapiValue, NapiError};
+use node_api::{NapiEnv, NapiValue, FromNapiValues, IntoNapiValue};
 use node_api::{create_function, set_named_property, create_object};
+use node_api::error::*;
 
 #[no_mangle]
-pub extern "C" fn register(env: NapiEnv,
-                           exports: NapiValue,
-                           _module: NapiValue,
-                           _priv: *mut std::os::raw::c_void) {
-    let function = create_function(env, "foo", |_: NapiEnv, _: HelloArgs| {
+pub extern "C" fn register(env: NapiEnv, exports: NapiValue, _module: NapiValue, _priv: *mut std::os::raw::c_void) {
+    let function = create_function(env, "foo", |_: NapiEnv, _: NapiValue, ()| {
         HelloReturn {
             foo: "hello".to_string(),
             bar: 42,
@@ -24,7 +22,7 @@ pub extern "C" fn register(env: NapiEnv,
 
 struct HelloArgs {}
 impl FromNapiValues for HelloArgs {
-    fn from_napi_args(_: NapiEnv, _: &[NapiValue]) -> Result<Self, NapiError> {
+    fn from_napi_values(_: NapiEnv, _: NapiValue,  _: &[NapiValue]) -> Result<Self> {
         Ok(HelloArgs {})
     }
 }
@@ -33,11 +31,11 @@ struct HelloReturn {
     pub foo: String,
     pub bar: u64,
 }
-impl ToNapiValue for HelloReturn {
-    fn to_napi_value(&self, env: NapiEnv) -> node_api::Result<NapiValue> {
+impl IntoNapiValue for HelloReturn {
+    fn into_napi_value(self, env: NapiEnv) -> Result<NapiValue> {
         let object = create_object(env)?;
-        let foo = self.foo.to_napi_value(env)?;
-        let bar = self.bar.to_napi_value(env)?;
+        let foo = self.foo.into_napi_value(env)?;
+        let bar = self.bar.into_napi_value(env)?;
         set_named_property(env, object, "foo", foo)?;
         set_named_property(env, object, "bar", bar)?;
         Ok(object)
